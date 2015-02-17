299281ec7ce0cf8e0531646b4790204a9cdac350
MWHanco <MWHanco@2355DQ4PK127V7.nam.ent.duke-energy.com>
2015-02-17 15:59:40 -0500

    Looks good to me.  I removed the "hasQuantity" not sure why I put that in the ontology.  DataTypeProperty "Quantity" does the job.

--- src/edu/ncsu/soc/project2/part2/Checker.java
+++ src/edu/ncsu/soc/project2/part2/Checker.java
@@ -1,10 +1,14 @@
 package edu.ncsu.soc.project2.part2;
 
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.Iterator;
+import java.util.Date;
 
 import com.hp.hpl.jena.rdf.model.InfModel;
+import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
@@ -16,7 +20,7 @@ import com.hp.hpl.jena.reasoner.Reasoner;
 import com.hp.hpl.jena.reasoner.ReasonerRegistry;
 import com.hp.hpl.jena.reasoner.ValidityReport;
 import com.hp.hpl.jena.reasoner.ValidityReport.Report;
-import com.hp.hpl.jena.util.PrintUtil;
+import com.hp.hpl.jena.datatypes.xsd.*;
 
 import android.app.Activity;
 import android.content.res.AssetManager;
@@ -26,6 +30,21 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 public class Checker extends Activity {
+	
+	final static String OWL_REF  = "http://csc750/p2/reference.owl#";
+	
+	String firstName = "";
+	String lastName = "";
+	String street = "";
+	String city = "";
+	String state = "";
+	String zip = "";
+	String itemName = "";	
+	Float unitPrice;
+	int orderNumber;
+	int quantity;	
+	Date orderDate;
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +69,7 @@ public class Checker extends Activity {
 				inputStream = assetManager.open("PO_new.owl");
 				schema.read(inputStream, null);
 				inputStream.close();
-				inputStream = assetManager.open("purchase_order.rdf");
+				inputStream = assetManager.open("purchase_order.owl");
 				data.read(inputStream, null);
 				inputStream.close();
 				
@@ -60,7 +79,78 @@ public class Checker extends Activity {
 				
 				ValidityReport validity = infmodel.validate();
 				if (validity.isValid()) {
-				    output = "OK";
+				    output = "Order Confirmed";
+				    
+				    StmtIterator iter = data.listStatements();
+					
+					// print out the predicate, subject and object of each statement
+					while (iter.hasNext())
+					{
+					    Statement stmt      = iter.nextStatement();  // get next statement
+					    Resource  subject   = stmt.getSubject();     // get the subject
+					    Property  predicate = stmt.getPredicate();   // get the predicate
+					    RDFNode   object    = stmt.getObject();      // get the object
+					    
+										    
+					    if(predicate.toString().equals(OWL_REF + "LastName"))
+					    	lastName = object.toString();
+					    else if (predicate.toString().equals(OWL_REF + "FirstName"))
+					    	firstName = object.toString();
+					    else if (predicate.toString().equals(OWL_REF + "OrderNumber")) {					   
+						   Literal l = (Literal)object;
+						   orderNumber = l.getInt();						   
+					    }
+					    else if (predicate.toString().equals(OWL_REF + "unitPrice")) {
+					    	Literal l = (Literal)object;
+					    	unitPrice = l.getFloat();
+					    }
+					    else if (predicate.toString().equals(OWL_REF + "Quantity")) {
+					    	Literal l = (Literal)object;
+					    	quantity = l.getInt();					    	
+					    }
+					    else if (predicate.toString().equals(OWL_REF + "OrderDate")) {
+					    	Literal l = (Literal)object;
+					    	XSDDateTime xdate = (XSDDateTime)l.getValue();
+					    	orderDate = xdate.asCalendar().getTime();
+					    }						 					    
+					    else if (predicate.toString().equals(OWL_REF + "Street"))
+					    	street = object.toString();
+					    else if (predicate.toString().equals(OWL_REF + "City"))
+					    	city = object.toString();
+					    else if (predicate.toString().equals(OWL_REF + "State"))
+					    	state = object.toString();
+					    else if(predicate.toString().equals(OWL_REF + "Zip"))
+					    	zip = object.toString();
+					    else if (predicate.toString().equals(OWL_REF + "ItemName"))
+					    	itemName = object.toString();
+					    
+//###########################################################################################
+//            UNCOMMENT TO VIEW SUBJECT/PREDICATE/OBJECT RESULTS
+//					    
+//					    output += " Subject: " + subject.toString();
+//					    output += " Predicate: " + predicate.toString() + " ";
+//					    if(object instanceof Resource)
+//					    {
+//					       output += " Object: " + object.toString() + ".\n\n";
+//					    } 
+//					    else{
+//					        // object is a literal
+//					        output += " Object:  \"" + object.toString() + "\"" + ".\n\n";
+//					    }
+//############################################################################################
+					    
+					}
+				
+					output += "\nOrder Number: " + orderNumber;					
+					output += "\nOrder Date: " + sdf.format(orderDate);
+					output += "\nFirst Name: " + firstName;
+					output += "\nLast Name: " + lastName;
+					output += "\nItem Name: " + itemName;
+					output += "\nQuantity: " + quantity;
+					output += "\nTotal Price: " + quantity * unitPrice;
+					output += "\nAddress: " + street + ", " + city + ", " + state + " " + zip;
+					//output += "\nUnit Price: " + unitPrice;	
+					
 				} else {
 				    output = "Conflicts";
 				    for (Iterator<Report> i = validity.getReports(); i.hasNext(); ) {
@@ -72,13 +162,11 @@ public class Checker extends Activity {
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}
-			
+			}			
 			
 			textView.setText(output);
-		
-	}
-	
+			System.out.println(output);
+	}	
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
